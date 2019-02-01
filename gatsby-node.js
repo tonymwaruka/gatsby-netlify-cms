faadb2a67f1b5f26b12acd35b74ae9d58307f726
Tony Mwaruka <tonymwaruka@icloud.com>
2019-02-01 15:15:33 -0700

    removed slug from from createFilePath

--- gatsby-node.js
+++ gatsby-node.js
@@ -80,9 +80,8 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
     createNodeField({
-      name: `id`,
       node,
-      value,
+      value
     })
   }
 }
